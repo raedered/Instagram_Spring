70c1337665675c5fcd4e59e6cfe43e1d86a3513e
raedered <kjh991216@naver.com>
2022-05-03 18:37:00 +0900

    instagram_spring 수정

--- instagram_spring/src/main/webapp/static/js/account.js
+++ instagram_spring/src/main/webapp/static/js/account.js
@@ -1,4 +1,4 @@
-const profileusername = document.querySelector(".profile-username");
+const profileUsername = document.querySelector(".profile-username");
 const textInputs = document.querySelectorAll(".text-input");
 const inctroduceText = document.querySelector(".text-textarea");
 const submitBtn = document.querySelector(".submit-btn");
@@ -7,13 +7,14 @@ const profileImgFile = document.querySelector(".profile-img-file");
 const profileImgRound = document.querySelector(".profile-img-round");
 const updateImgBtn = document.querySelector(".update-img-button");
 
+
 let usercode = 0;
 
 let principal = getPrincipal();
 
 load();
 
-function load(){
+function load() {
 	$.ajax({
 		type: "get",
 		url: "/app/profile/account/user",
@@ -21,20 +22,21 @@ function load(){
 			usercode : principal.usercode
 		},
 		dataType: "text",
-		success: function(data) {
+		success: function(data){
 			let account = JSON.parse(data);
 			pageLoad(account);
 		},
-		error: function() {
+		error: function(){
 			alert("비동기 처리 오류");
 		}
-	})
+	});
+	const profileImg = profileImgRound.querySelector('img');
+	profileImg.src = getProfileImg();
 }
-
 
 function pageLoad(account){
 	usercode = account.usercode;
-	profileusername.textContent = account.username;
+	profileUsername.textContent = account.username;
 	textInputs[0].value = account.name;
 	textInputs[1].value = account.username;
 	textInputs[2].value = account.website;
@@ -54,8 +56,8 @@ submitBtn.onclick = () => {
 		dataType: "text",
 		success: function(data){
 			if(data == "true"){
-				alert("회원정보 수정 완료.")
-				location.replace("/app/profile/account")
+				alert("회원정보 수정 완료.");
+				location.replace("/app/profile/account");
 			}else {
 				alert("이미 존재하는 사용자 이름입니다.");
 			}
@@ -63,19 +65,20 @@ submitBtn.onclick = () => {
 		error: function(){
 			alert("비동기 처리 오류");
 		}
+		
 	});
 }
 
 function createAccount(){
 	let account = {
-		"usercode" : usercode,
-		"name" : textInputs[0].value,
-		"username" : textInputs[1].value,
-		"website" : textInputs[2].value,
-		"introduce" : inctroduceText.value,
-		"email" : textInputs[3].value,
-		"phone" : textInputs[4].value,
-		"gender" : textInputs[5].value
+		"usercode": usercode,
+		"name": textInputs[0].value,
+		"username": textInputs[1].value,
+		"website": textInputs[2].value,
+		"introduce": inctroduceText.value,
+		"email": textInputs[3].value,
+		"phone": textInputs[4].value,
+		"gender": textInputs[5].value
 	}
 	
 	return account;
@@ -96,38 +99,44 @@ function imgChange() {
 		
 		reader.onload = (e) => {
 			let profileImgUrl = e.target.result;
-			let originImgUrl = profileImgRound.querySelector("img").src;
-			profileImgRound.querySelector("img").src = profileImgUrl;
+			let originImgUrl = profileImgRound.querySelector('img').src;
+			profileImgRound.querySelector('img').src = profileImgUrl;
 			
 			if(confirm("프로필 이미지를 변경하시겠습니까?")){
 				let formData = new FormData(document.querySelector(".profile-box-form1"));
+				
 				$.ajax({
 					type: "post",
 					url: "/app/profile/account/update/img",
 					data: formData,
 					encType: "multipart/form-data",
-					processData: false,			//multipart 사용할때는 encType,processData,contentType를 함께 사용해야한다.
+					processData: false,
 					contentType: false,
-					dataType:"text",
-					success: function(data){
-						alert("프로필 이미지가 변경되었습니다.")
+					dataType: "text",
+					success: function(data) {
+						alert("프로필 이미지가 변경되었습니다.");
+						location.reload();
 					},
 					error: function(){
-						alert("비동기 처리 오류")
+						alert("비동기 처리 오류.");
 					}
 				});
 			}else{
-				profileImgRound.querySelector("img").src = originImgUrl;
+				profileImgRound.querySelector('img').src = originImgUrl;
 			}
 		}
 		
 		reader.readAsDataURL(profileImgFile.files[0]);
-	}	
-}
-
-function showconfirm(){
+	}
 	
 }
+
+
+
+
+
+
+
 
 
 
---
+++ instagram_spring/target/app-1.0.0-BUILD-SNAPSHOT/static/js/nav.js
@@ -0,0 +1,10 @@
+const profileImg = document.querySelector(".profile-img");
+const bLogo = document.querySelector(".b-logo");
+
+bLogo.onclick = () => {
+	location.href = "/app/";
+}
+
+profileImg.onclick = () => {
+	location.href = "/app/account/profile";
+}
